1a8375295dd43a4f8886151ac12800d057862a26
Gabriel <gabriellf.dev@gmail.com>
2022-12-22 16:48:26 -0300

    portifolio completo

---
+++ src/utils/stackData.ts
@@ -0,0 +1,54 @@
+import { TbBrandNextjs } from "react-icons/tb";
+import {
+  SiPostgresql,
+  SiTypescript,
+  SiDjango,
+  SiNestjs,
+  SiStyledcomponents,
+  SiMaterialui,
+  SiReactrouter,
+  SiTailwindcss,
+  SiExpress,
+} from "react-icons/si";
+import { FaGit, FaPython } from "react-icons/fa";
+import { FaHtml5, FaCss3Alt, FaJs, FaNode, FaReact } from "react-icons/fa";
+import { DiScrum } from "react-icons/di";
+
+export const stackData = [
+  { title: "Next.js", img: TbBrandNextjs },
+  {
+    title: "React",
+    img: FaReact,
+  },
+  { title: "React Router Dom", img: SiReactrouter },
+  { title: "Material Ui", img: SiMaterialui },
+  { title: "Styled Components", img: SiStyledcomponents },
+  { title: "Tailwindcss", img: SiTailwindcss },
+  { title: "TypeScript", img: SiTypescript },
+  {
+    title: "HTML",
+    img: FaHtml5,
+  },
+  {
+    title: "CSS",
+    img: FaCss3Alt,
+  },
+  {
+    title: "JS",
+    img: FaJs,
+  },
+  { title: "Git", img: FaGit },
+  { title: "Scrum", img: DiScrum },
+  { title: "PostgreSQL", img: SiPostgresql },
+  { title: "Python", img: FaPython },
+  { title: "Django", img: SiDjango },
+  {
+    title: "Node JS",
+    img: FaNode,
+  },
+  {
+    title: "Express",
+    img: SiExpress,
+  },
+  { title: "NestJs", img: SiNestjs },
+];
---
+++ src/utils/userData.ts
@@ -0,0 +1,9 @@
+export const userData = {
+  nameUser: "Gabriel Fernandes",
+  githubUser: "gabriellfernandes",
+  linkedinUser: "gabriel-lima-fernandes",
+  whatsappNumber: "+551999112119",
+  emailUser: "gabriellf.dev@gmail.com",
+  instagramUser: "",
+  facebookUser: "",
+};
